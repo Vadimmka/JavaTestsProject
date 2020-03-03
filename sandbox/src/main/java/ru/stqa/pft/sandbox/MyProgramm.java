359c586846dc8408a9557f56fca454ca2a49ee27
Vadim Vasilyev <vad9531620853@yandex.ru>
2020-03-03 21:55:08 +0300

    Reset to math operations

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyProgramm.java
+++
@@ -1,9 +0,0 @@
-package ru.stqa.pft.sandbox;
-
-public class MyProgramm {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-
-		System.out.println(2 + 2);
-    }
-}
