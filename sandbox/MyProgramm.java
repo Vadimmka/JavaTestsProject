589b9f05ab11a790482f97324c6eda69453f8f31
Vadim Vasilyev <vad9531620853@yandex.ru>
2020-03-03 22:32:03 +0300

    Revert "Revert "Add math operations""
    This reverts commit a62eb9cb0709ed6b0e87e366e5eb39d849276f72.

--- sandbox/MyProgramm.java
+++ sandbox/MyProgramm.java
@@ -1,6 +1,7 @@
 public class MyProgramm {
-	
-	public static void main(String[] args ) {
-		System.out.println("Hello World!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+		System.out.println(2 + 2);
+    }
 }
